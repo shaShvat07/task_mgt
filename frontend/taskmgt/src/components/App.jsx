584c8af4bc1ea2a48b0e6b5fcfff10951972218b
Shashvat <shashvatpatel2803@gmail.com>
2024-05-08 15:33:22 +0530

    Folder Structure Initiated

--- frontend/taskmgt/src/components/App.jsx
+++ frontend/taskmgt/src/components/App.jsx
@@ -1,13 +1,22 @@
-
 import './App.css'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './Login/Login';
+import SignUp from './SignUp/SignUp';
+import Home from './Home/Home';
+import PageNotFound from './PageNotFound/PageNotFound';
 
 function App() {
 
   return (
     <>
-        <div> 
-          Hello World
-        </div>
+      <BrowserRouter>
+        <Routes >
+          <Route index element={<Home />} />
+          <Route path="login" element={<Login />} />
+          <Route path="register" element={<SignUp />} />
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
