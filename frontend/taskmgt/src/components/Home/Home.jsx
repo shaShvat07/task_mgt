2f71a3be1c38e2e2405c88ffa2a51f74aebe5e6c
Shashvat <shashvatpatel2803@gmail.com>
2024-05-09 17:18:39 +0530

    Feature Added: Added Landing Page + Responsiveness

--- frontend/taskmgt/src/components/Home/Home.jsx
+++ frontend/taskmgt/src/components/Home/Home.jsx
@@ -1,13 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react';
 import LandingPage from '../LandingPage/LandingPage';
 import Dashboard from '../Dashboard/Dashboard';
 
 const Home = () => {
   const Auth = false;
+  const [bgColor, setBgColor] = useState('bg-white');
+  const handleButton1Hover = () => {
+    setBgColor('bg-fuchsia-500');
+  };
+
+  const handleButton2Hover = () => {
+    setBgColor('bg-orange-400');
+  };
+
   return (
-    <>
-      {Auth ? <Dashboard /> : <LandingPage />}
-    </>
+    <div className='h-full'>
+      {Auth ? <Dashboard /> :
+        <>
+          <div className={`w-full ${bgColor}  h-16 flex text-2xl`}>
+            <a
+              className="w-1/2 flex justify-end items-center pr-5 hover:cursor-pointer"
+              onMouseEnter={handleButton1Hover}
+              onMouseLeave={() => setBgColor('bg-white')}
+              href='/login'
+            >
+              Sign In
+            </a>
+            <a
+              className="w-1/2 flex justify-start items-center pl-5 hover:cursor-pointer"
+              onMouseEnter={handleButton2Hover}
+              onMouseLeave={() => setBgColor('bg-white')}
+              href='register'
+            >
+              Register
+            </a>
+          </div>
+          <LandingPage />
+        </>
+      }
+    </div>
   )
 }
 
--- frontend/taskmgt/src/components/LandingPage/LandingPage.jsx
+++ frontend/taskmgt/src/components/LandingPage/LandingPage.jsx
@@ -1,9 +1,46 @@
-import React from 'react'
-
+import React, { useEffect, useRef } from 'react';
+import './LandingPage.css';
 const LandingPage = () => {
+  const leftSideRef = useRef(null);
+
+  useEffect(() => {
+    const handleMove = (e) => {
+      const leftSide = leftSideRef.current;
+      if (leftSide) {
+        leftSide.style.width = `${e.clientX / window.innerWidth * 100}%`;
+      }
+    };
+
+    const handleTouchMove = (e) => {
+      handleMove(e.touches[0]);
+    };
+
+    window.addEventListener('mousemove', handleMove);
+    window.addEventListener('touchmove', handleTouchMove);
+
+    return () => {
+      window.removeEventListener('mousemove', handleMove);
+      window.removeEventListener('touchmove', handleTouchMove);
+    };
+  }, []);
+
   return (
-    <div>LandingPage</div>
-  )
-}
+    <>
+      <div id="left-side" class="side" ref={leftSideRef}>
+        <h2 class="title">
+           Empower Your Productivity Journey with 
+          <span class="fancy">TaskMgt</span>
+        </h2>
+      </div>
+      <div id="right-side" class="side">
+        <h2 class="title">
+        Empower Your Productivity Journey with 
+          <span class="fancy">TaskMgt</span>
+        </h2>
+      </div>
+    </>
+  );
+};
+
 
 export default LandingPage;
