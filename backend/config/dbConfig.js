d8eeae2d49496b6d819d15dbeb6d0186e24bd93f
Shashvat <shashvatpatel2803@gmail.com>
2024-05-11 02:31:54 +0530

    Added vercel config file in backend

--- backend/config/dbConfig.js
+++ backend/config/dbConfig.js
@@ -1,14 +1,14 @@
-const pg = require("pg");
 const dotenv = require('dotenv');
-
+const { Pool } = require('pg');
 dotenv.config();
 
-const pool = new pg.Pool({
-  user: process.env.DB_USER,
-  password: process.env.DB_PASSWORD,
-  host: process.env.DB_HOST,
-  port: process.env.DB_PORT,
-  database: process.env.DB_NAME,
+const pool = new Pool({
+  connectionString: process.env.POSTGRES_URL,
+});
+
+pool.connect((err) => {
+  if(err) throw err;
+  console.log("Connected to PostgreSQL successfully!");
 });
 
 module.exports = { pool };
